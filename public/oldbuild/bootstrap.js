c5f0fe06d532a993581baea018f1dd628ab87476
François JOUBAUD <dracofanfan@hotmail.com>
2023-10-04 08:30:06 +0200

    Ajout du script Encore

---
+++ public/oldbuild/bootstrap.js
@@ -0,0 +1,8 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["bootstrap"],{},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ __webpack_require__.O(0, ["vendors-node_modules_bootstrap_dist_js_bootstrap_esm_js"], () => (__webpack_exec__("./node_modules/bootstrap/dist/js/bootstrap.esm.js")));
+/******/ var __webpack_exports__ = __webpack_require__.O();
+/******/ }
+]);
